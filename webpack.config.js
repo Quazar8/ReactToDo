18688187fa382de2d7d086107e1fbb9fa07cdd24
Quazar8 <junkyard12d@gmail.com>
2020-10-27 13:27:36 +0200

    Adding initial files and webpack to the project
    Webpack working with the initial fles; creating webpack config file;
    using html-webpack-plugin; modifying gitignore;

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const webpack = require('webpack')
+const HtmlPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+    entry:'./src/index.js',
+    output: {
+        path: path.resolve(__dirname,'dist'),
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new HtmlPlugin({
+            template: './src/index.html'
+        })
+    ]
+}
