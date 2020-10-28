48bf596b095cb4946b09efc008f213529216506e
Quazar8 <junkyard12d@gmail.com>
2020-10-28 16:03:37 +0200

    Adding an Input component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Input from './Input'
 
 const App = (props) => {
     return (
         <div>
             <h1>Welcome to React</h1>
+            <Input />
         </div>
     )
 }
