43bc1b65094aa224431a0c522990cffca0fc8489
Quazar8 <junkyard12d@gmail.com>
2020-10-29 15:55:55 +0200

    Adding css to single-todo component

--- src/components/SingleTodo.js
+++ src/components/SingleTodo.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import '../styles/single-todo.css'
 
 const SingleToDo = (props) => {
     return (
-        <h2>{props.todo}</h2>
+        <article className="single-todo">
+            <h2>{props.todo}</h2>
+            <button>X</button>
+        </article>
     )
 }
 
